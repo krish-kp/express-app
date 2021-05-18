6e6c54a283d00eb4511a4d58bb28488f465514ac
krish-kp <krish.kp1@gmail.com>
2021-05-17 21:42:16 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Adapoda' });
+  res.render('index', { title: 'Nirmata' });
 });
 
 module.exports = router;
